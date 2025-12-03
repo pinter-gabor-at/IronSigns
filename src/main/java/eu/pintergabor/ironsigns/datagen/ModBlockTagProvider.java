7f404b8e22b37f1bdfa545ba269302bb0c5011a5
Pintér Gábor <pinter.gabor@gmx.at>
2025-12-03 21:04:33 +0100

    Bump versions.

--- src/main/java/eu/pintergabor/ironsigns/datagen/ModBlockTagProvider.java
+++ src/main/java/eu/pintergabor/ironsigns/datagen/ModBlockTagProvider.java
@@ -2,13 +2,14 @@ package eu.pintergabor.ironsigns.datagen;
 
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
-import java.util.stream.IntStream;
 
 import eu.pintergabor.ironsigns.main.Main;
 import eu.pintergabor.ironsigns.main.SignVariant;
 
 import net.minecraft.core.HolderLookup;
+import net.minecraft.data.tags.TagAppender;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Block;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
@@ -23,30 +24,31 @@ public final class ModBlockTagProvider extends FabricTagProvider.BlockTagProvide
 		super(output, registriesFuture);
 	}
 
-	@Override
-	protected void addTags(HolderLookup.Provider wrapperLookup) {
-		FabricTagBuilder tagBuilder =
-			getOrCreateTagBuilder(Main.IRON_SIGN_BLOCK_TAG);
-		// Iron sign.
-		builderAdd(tagBuilder, Main.ironSign);
-		// Color signs.
-		Arrays.stream(Main.colorSigns)
-			.forEach(sv -> builderAdd(tagBuilder, sv));
-		// Make them mineable with axe and pickaxe.
-		getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_AXE)
-			.forceAddTag(Main.IRON_SIGN_BLOCK_TAG);
-		getOrCreateTagBuilder(BlockTags.MINEABLE_WITH_PICKAXE)
-			.forceAddTag(Main.IRON_SIGN_BLOCK_TAG);
-	}
 
 	@SuppressWarnings("UnusedReturnValue")
-	private FabricTagBuilder builderAdd(
-		FabricTagBuilder tagBuilder, SignVariant sv
+	private TagAppender<Block, Block> builderAdd(
+		TagAppender<Block, Block> tagBuilder, SignVariant sv
 	) {
 		return tagBuilder
 			.add(sv.standingSign)
 			.add(sv.wallSign)
 			.add(sv.ceilingHangingSign)
 			.add(sv.wallHangingSign);
+	}
+
+	@Override
+	protected void addTags(HolderLookup.Provider wrapperLookup) {
+		TagAppender<Block, Block> tagBuilder =
+			valueLookupBuilder(Main.IRON_SIGN_BLOCK_TAG);
+		// Iron sign.
+		builderAdd(tagBuilder, Main.ironSign);
+		// Color signs.
+		Arrays.stream(Main.colorSigns)
+			.forEach(sv -> builderAdd(tagBuilder, sv));
+		// Make them mineable with axe and pickaxe.
+		valueLookupBuilder(BlockTags.MINEABLE_WITH_AXE)
+			.addTag(Main.IRON_SIGN_BLOCK_TAG);
+		valueLookupBuilder(BlockTags.MINEABLE_WITH_PICKAXE)
+			.addTag(Main.IRON_SIGN_BLOCK_TAG);
 	}
 }
