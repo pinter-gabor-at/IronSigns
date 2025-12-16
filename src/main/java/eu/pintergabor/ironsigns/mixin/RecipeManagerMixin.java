0bbea394593455a9716219c64eb6b45b7bafb4b1
Pintér Gábor <pinter.gabor@gmx.at>
2025-12-16 16:28:39 +0100

    Bump versions.
    Tested only with an alpha ModMenu.

--- src/main/java/eu/pintergabor/ironsigns/mixin/RecipeManagerMixin.java
+++ src/main/java/eu/pintergabor/ironsigns/mixin/RecipeManagerMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.item.crafting.Recipe;
@@ -32,7 +32,7 @@ public abstract class RecipeManagerMixin {
 		ResourceManager resourceManager,
 		ProfilerFiller profiler,
 		CallbackInfoReturnable<RecipeMap> cir,
-		@Local SortedMap<ResourceLocation, Recipe<?>> sortedMap
+		@Local SortedMap<Identifier, Recipe<?>> sortedMap
 	) {
 		RecipeManagerUtil.configRecipes(sortedMap);
 	}
