a3c2accf6406ee8d2aa705be6634f62a4f86004b
Pintér Gábor <pinter.gabor@gmx.at>
2025-12-16 16:36:56 +0100

    Bump versions.

--- src/main/java/eu/pintergabor/ironsigns/Global.java
+++ src/main/java/eu/pintergabor/ironsigns/Global.java
@@ -1,9 +1,11 @@
 package eu.pintergabor.ironsigns;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 
 public final class Global {
@@ -28,8 +30,9 @@ public final class Global {
 	 *
 	 * @param path Name without {@link #MODID}.
 	 */
+	@Contract(pure = true)
 	@SuppressWarnings("unused")
-	public static String modName(String path) {
+	public static @NotNull String modName(String path) {
 		return MODID + ":" + path;
 	}
 
@@ -39,7 +42,7 @@ public final class Global {
 	 * @param path Name without {@link #MODID}.
 	 */
 	@SuppressWarnings("unused")
-	public static ResourceLocation modId(String path) {
-		return ResourceLocation.fromNamespaceAndPath(MODID, path);
+	public static Identifier modId(String path) {
+		return Identifier.fromNamespaceAndPath(MODID, path);
 	}
 }
